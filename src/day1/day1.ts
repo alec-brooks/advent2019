cbcf97e72ee37b048ff72f96a64840b66073e196
Alec Brooks <alec.e.brooks@gmail.com>
2019-12-01 20:27:55 +1100

    Recursive Day1 solution

--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,13 +1,9 @@
-export default (): void => {};
-
 export const fuelRequiredForMass = (mass: number): number => Math.floor(mass / 3) - 2;
 
 export const fuelRequiredForRocketMass = (mass: number): number => {
-  let fuelNeeded = fuelRequiredForMass(mass);
-  let fuelForFuel = fuelRequiredForMass(fuelNeeded);
-  while (fuelForFuel > 0) {
-    fuelNeeded += fuelForFuel;
-    fuelForFuel = fuelRequiredForMass(fuelForFuel);
+  const currentMassFuelWeight = fuelRequiredForMass(mass);
+  if (currentMassFuelWeight < 0) {
+    return 0;
   }
-  return fuelNeeded;
+  return currentMassFuelWeight + fuelRequiredForRocketMass(currentMassFuelWeight);
 };
