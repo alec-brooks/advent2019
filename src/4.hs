35b3ffb9c24ace483e878d39f06656a01ed1e3f7
Alec Brooks <abrooks@squareup.com>
2019-12-07 17:48:24 +1100

    Part 2 day 4

--- src/4.hs
+++ src/4.hs
@@ -3,6 +3,9 @@ import Data.List
 validPassword :: [Int] -> Bool
 validPassword password = (onlyIncrease password) && (containsRepeatingDigits password)
 
+validPasswordPart2 :: [Int] -> Bool
+validPasswordPart2 password = (onlyIncrease password) && (contains2RepeatingDigits password)
+
 allTheSame :: (Eq a) => [a] -> Bool
 allTheSame xs = and $ map (== head xs) (tail xs)
 
@@ -14,8 +17,12 @@ allPairs password =
   (chunks 2 password) ++ (chunks 2 $ init  $ tail password)
 
 containsRepeatingDigits :: [Int] -> Bool
-containsRepeatingDigits password = 
+containsRepeatingDigits password =
   any allTheSame $ allPairs password
+
+contains2RepeatingDigits :: [Int] -> Bool
+contains2RepeatingDigits password =
+  any allTheSame $ (unique . allPairs) password
 
 onlyIncrease :: [Int] -> Bool
 onlyIncrease password = 
@@ -26,7 +33,7 @@ digits n = map (\x -> read [x] :: Int) (show n)
 
 countValidPasswords :: [[Int]] -> Int
 countValidPasswords = 
-  foldl (\count p -> if (validPassword p) then count + 1 else count) 0  
+  foldl (\count p -> if (validPasswordPart2 p) then count + 1 else count) 0
   
 filterByLength :: Ord a => (Int -> Bool) -> [a] -> [[a]]
 filterByLength p = filter (p . length) . group . sort
@@ -38,10 +45,7 @@ windows m = foldr (zipWith (:)) (repeat []) . take m . tails
 
 main :: IO ()
 main = do
-  print $ map (unique . allPairs) $ windows 4 [1,1,1,4,5,6]
-  -- print $ unique $ allPairs [1,1,2,2,1,1]
-  -- print $ unique $ allPairs [1,2,2,2,4,1]
-  -- print $ countValidPasswords $ map digits [353096..843212]
+  print $ countValidPasswords $ map digits [353096..843212]
   -- print $ validPassword [1,1,1,1,1,1]
   -- print $ validPassword [2,2,3,4,5,0]
   -- print $ validPassword [1,2,3,7,8,9]
